9cd55f4d9d63a7f64288a67a5927094ab2b8e173
Rob Williams <robdwilliams85@gmail.com>
2015-03-06 21:27:54 +0000

    added images

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,10 +1,10 @@
 class PinsController < ApplicationController
-  before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
 
   respond_to :html
-
+  
   def index
     @pins = Pin.all
     respond_with(@pins)
@@ -23,7 +23,7 @@ class PinsController < ApplicationController
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.new(pin_params)
     @pin.save
     respond_with(@pin)
   end
@@ -37,18 +37,19 @@ class PinsController < ApplicationController
     @pin.destroy
     respond_with(@pin)
   end
-
+  
   private
     def set_pin
       @pin = Pin.find(params[:id])
     end
 
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
-    
-  def correct_user
-    @pin = current_user.pins.find_by(id: params[:id])
-    redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
-  end
+   
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
+
 end
